5755bb6b129f2a5ddc7f6030ca89256b85424579
David-mwas <dmwas704@gmaIl.com>
2024-10-09 09:29:30 +0300

    Update -> login

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Login() {
   return (
     <View className="bg-green-900 flex-1">
-      <Text>Login page</Text>
+      <Text>Login</Text>
     </View>
   );
 }
