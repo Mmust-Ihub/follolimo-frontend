f9ad967b95bfd84acda9c6278cf4a01027ab26c6
Muchael123 <mainamichael2849@gmail.com>
2024-10-12 20:11:55 +0300

    Add(Core): Add calendar

--- app/(tabs)/inventory/Calendar.tsx
+++ app/(tabs)/inventory/Calendar.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MonthPicker from '@/app/components/calendar/MonthPicker'
 
 export default function Calendar() {
   return (
     <View>
-      <Text>Calendar</Text>
+      <MonthPicker />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+})
