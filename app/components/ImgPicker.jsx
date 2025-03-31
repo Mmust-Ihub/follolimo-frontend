3e83f8e71b61296d10bfa0669ab4ae3ce456c68f
David-mwas <dmwas704@gmail.com>
2025-03-31 16:50:15 +0300

    Update Backened Url

--- app/components/ImgPicker.jsx
+++ app/components/ImgPicker.jsx
@@ -44,7 +44,7 @@ export default function ImagePickerExample() {
 
     try {
       const res = await fetch(
-        `https://fololimo-api.vercel.app/api/v1/model/pest`,
+        `${process.env.EXPO_PUBLIC_NODEAPI_URL}/model/pest`,
         {
           method: "POST",
           headers: {
